9acb41bf0b9dc90fe4188eef913a0a5503db66cb
Oleg Shalygin <oshalygin@gmail.com>
2016-08-30 23:05:21 -0700

    building out note component

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -9,6 +9,7 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/nock/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react-router/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
