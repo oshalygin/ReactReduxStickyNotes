3a4ae0f44f73ef4b32ceb2fbfd4a3a5e68b2c624
Oleg Shalygin <oshalygin@gmail.com>
2016-09-05 01:31:51 -0700

    refactoring note component

--- client/components/board/note.jsx
+++ client/components/board/note.jsx
@@ -1,47 +1,10 @@
 import React, { PropTypes } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as stickyNoteActions from "../../actions/stickyNoteActions.js";
 import ReactDOM from "react-dom"; //eslint-disable-line no-unused-vars
 import Draggable from "react-draggable";
 import TextArea from "../common/textArea.jsx";
-
-export function setTextDirection(condition) {
-    return condition ? { transform: "rotateY(-180deg)" } : {};
-}
-
-export function renderDisplay(note, onEdit, onRemove, moveToCenter) {
-    const textDirection = setTextDirection(note.centered);
-    return (
-        <Draggable>
-            <div className="note-container">
-                <div className="note"
-                    style={Object.assign({}, note.position) }>
-                    <p style={textDirection}>{note.text}</p>
-                    <span>
-                        <button onClick={() => moveToCenter(note.id) }
-                            className="btn btn-warning glyphicon glyphicon-resize-full"/>
-                        <button onClick={() => onEdit(note.id) }
-                            className="btn btn-primary glyphicon glyphicon-pencil"/>
-                        <button onClick={onRemove}
-                            className="btn btn-danger glyphicon glyphicon-trash"/>
-                    </span>
-                </div>
-            </div>
-        </Draggable>
-    );
-}
-
-export function renderForm(note, onSave, onChange) {
-    return (
-        <Draggable>
-            <div className="note" style={note.position}>
-                <TextArea
-                    id={note.id}
-                    text={note.text}
-                    onChange={onChange} />
-                <button onClick={() => onSave(note.id) } className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
-            </div>
-        </Draggable>
-    );
-}
 
 
 class Note extends React.Component {
@@ -49,6 +12,7 @@ class Note extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
+            notes: props.notes,
             note: props.note,
             onSave: props.onSave,
             onEdit: props.onEdit,
@@ -57,26 +21,28 @@ class Note extends React.Component {
             onExpand: props.onExpand
         };
         this.moveToCenter = this.moveToCenter.bind(this);
-        // this.onNoteTextChange = this.onNoteTextChange.bind(this);
+        this.onNoteTextChange = this.onNoteTextChange.bind(this);
+        this.onEdit = this.onEdit.bind(this);
     }
 
     componentDidUpdate() {
 
     }
 
+    componentWillReceiveProps(newProps) {
+        this.setState({ note: newProps.note });
+    }
+
     onNoteTextChange(event) {
-        // console.log("onNoteTextChange");
-        // const noteId = event.target.name;
+        let {note} = this.props;
+        note.text = event.target.value;
+        return this.setState({});
+    }
 
-        // let changedNote = this.props.notes
-        //     .filter(note => note.id === noteId)[0];
-        // changedNote.text = event.target.value;
-
-        // let updatedNotes = [...this.props.notes];
-        // let existingNoteIndex = updatedNotes.findIndex(note => note.id === noteId);
-        // updatedNotes.splice(existingNoteIndex, 1, Object.assign({}, changedNote));
-
-        // return this.setState({});
+    onEdit() {
+        console.log("onEdit");
+        const {note} = this.props;
+        this.props.stickyNoteActions.editingNote(Object.assign({}, note));
     }
 
     getPosition() {
@@ -87,26 +53,84 @@ class Note extends React.Component {
         const position = ReactDOM.findDOMNode(this).firstChild.getBoundingClientRect();
         console.log(position);
         this.props.onExpand(noteId, position);
+        this.setState({});
     }
 
     render() {
 
-        const {note, onSave, onChange, onEdit, onRemove} = this.state;
+        function setTextDirection(condition) {
+            return condition ? { transform: "rotateY(-180deg)" } : {};
+        }
+
+        function renderDisplay(note, onEdit, onRemove, moveToCenter) {
+            const textDirection = setTextDirection(note.centered);
+            return (
+                <Draggable>
+                    <div className="note-container">
+                        <div className="note"
+                            style={Object.assign({}, note.position) }>
+                            <p style={textDirection}>{note.text}</p>
+                            <span>
+                                <button onClick={() => moveToCenter(note.id) }
+                                    className="btn btn-warning glyphicon glyphicon-resize-full"/>
+                                <button onClick={onEdit} disabled={note.centered}
+                                    className="btn btn-primary glyphicon glyphicon-pencil"/>
+                                <button onClick={onRemove} disabled={note.centered}
+                                    className="btn btn-danger glyphicon glyphicon-trash"/>
+                            </span>
+                        </div>
+                    </div>
+                </Draggable>
+            );
+        }
+
+        function renderForm(note, onSave, onChange) {
+            return (
+                <Draggable>
+                    <div className="note-container">
+                        <div className="note"
+                            style={Object.assign({}, note.position) }>
+                            <TextArea
+                                id={note.id}
+                                text={note.text}
+                                onChange={onChange} />
+                            <button onClick={() => onSave(note.id) } className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+                        </div>
+                    </div>
+                </Draggable>
+            );
+        }
+
+        const {note, onSave, onRemove} = this.state;
         if (!!note.editMode) {
-            return renderForm(note, onSave, onChange);
+            return renderForm(note, onSave, this.onNoteTextChange);
         } else { //eslint-disable-line
-            return renderDisplay(note, onEdit, onRemove, this.moveToCenter);
+            return renderDisplay(note, this.onEdit, onRemove, this.moveToCenter);
         }
     }
 }
 
 Note.propTypes = {
     note: PropTypes.object.isRequired,
+    notes: PropTypes.array.isRequired,
     onSave: PropTypes.func.isRequired,
     onEdit: PropTypes.func.isRequired,
     onChange: PropTypes.func.isRequired,
     onRemove: PropTypes.func.isRequired,
-    onExpand: PropTypes.func.isRequired
+    onExpand: PropTypes.func.isRequired,
+    stickyNoteActions: PropTypes.object.isRequired
 };
 
-export default Note;
+function mapStateToProps(state) {
+    return {
+        notes: state.notes
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        stickyNoteActions: bindActionCreators(stickyNoteActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Note);
